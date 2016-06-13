0eb8709b1b49e9fd21d1dfccf2109509915a6db4
Prestanesia <prestanesia@gmail.com>
2016-06-13 13:36:46 +0700

    Initial project

---
+++ override/controllers/admin/AdminOrdersController.php
@@ -0,0 +1,10 @@
+<?php
+
+class AdminOrdersController extends AdminOrdersControllerCore
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->addRowAction('delete');
+    }
+}
